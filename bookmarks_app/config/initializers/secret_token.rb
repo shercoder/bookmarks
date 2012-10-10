21d423bce5a261318def8e5b98f9174ee6faac72
Pardeep Singh <psingh16@mail.csuchico.edu>
2012-10-09 23:26:19 -0700

    Created rails app for project 4

---
+++ bookmarks_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookmarksApp::Application.config.secret_token = 'e2c1475b9b3dcf1eec43186cecbc6c68c0181500fcdd423d0a399a0f6f83af52e12bb98ef3076576881d7acd49eceb8cbf8a191e24283b607b34f941583c6131'
