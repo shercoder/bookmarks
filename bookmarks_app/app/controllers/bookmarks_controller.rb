5229944d7f20e6e773bb954eca9dd8a11a96a8e2
Pardeep Singh <psingh16@mail.csuchico.edu>
2012-10-14 01:14:42 -0700

    Adds users and bookmarks controller with 7 empty actions, adds few empty views for controller actions, adds images and css files from mockup to assets folder, adds resources for the controller actions, deletes public index html file, adds bookmarks/index.html as the root

---
+++ bookmarks_app/app/controllers/bookmarks_controller.rb
@@ -0,0 +1,31 @@
+class BookmarksController < ApplicationController
+
+	# GET => /bookmarks
+	def index
+	end
+
+	# GET => /bookmarks/:id
+	def show
+	end
+
+	# GET => /bookmarks/new
+	def new
+	end
+
+	# POST => /bookmarks/
+	def create
+	end
+
+	# GET => /bookmarks/:id/edit
+	def edit
+	end
+
+	# PUT => /bookmarks/:id
+	def update
+	end
+
+	# DELETE => /bookmarks/:id
+	def destroy
+	end
+
+end
---
+++ bookmarks_app/app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+
+	# GET => /users
+	def index
+	end
+
+	# GET => /users/:id
+	def show
+	end
+
+	# GET => /users/new
+	def new
+	end
+
+	# POST => /users/
+	def create
+	end
+
+	# GET => /users/:id/edit
+	def edit
+	end
+
+	# PUT => /users/:id
+	def update
+	end
+
+	# DELETE => /users/:id
+	def destroy
+	end
+
+end
